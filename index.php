8ea357ac9cb40d93c2edd11e3fcce4683fcd8f70
ZhangXQA <z1304242002@163.com>
2022-09-14 09:55:54 +0800

    自动分页完成

--- index.php
+++ index.php
@@ -91,259 +91,86 @@ $p = $_REQUEST['p'];
     <?php
     if (!$p)
     {
+        $p = 1;
     ?>
-    <!-- 轮播图 -->
-    <div id="carouselExampleCaptions" class="container carousel slide" data-ride="carousel" width="1920px">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div id="carousel-js" class="carousel-item active">
-                <div class="progressive">
-                    <img data-src="https://bing.nxingcloud.co/api/?day=0" src="https://bing.nxingcloud.co/api/?day=0&thumbnail=1"
-                        class="d-block w-100 rounded preview lazy" alt="bing">
+        <!-- 轮播图 -->
+        <div id="carouselExampleCaptions" class="container carousel slide" data-ride="carousel" width="1920px">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div id="carousel-js" class="carousel-item active">
+                    <div class="progressive">
+                        <img data-src="https://bing.nxingcloud.co/api/?day=0" src="https://bing.nxingcloud.co/api/?day=0&thumbnail=1"
+                            class="d-block w-100 rounded preview lazy" alt="bing">
+                    </div>
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5></h5>
+                        <p></p>
+                    </div>
                 </div>
-                <div class="carousel-caption d-none d-md-block">
-                    <h5></h5>
-                    <p></p>
+                <div class="carousel-item">
+                    <img src="https://bing.nxingcloud.co/api/?day=1" class="d-block w-100 rounded" alt="bing">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5></h5>
+                        <p></p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="https://bing.nxingcloud.co/api/?day=2" class="d-block w-100 rounded" alt="bing">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5></h5>
+                        <p></p>
+                    </div>
                 </div>
             </div>
-            <div class="carousel-item">
-                <img src="https://bing.nxingcloud.co/api/?day=1" class="d-block w-100 rounded" alt="bing">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5></h5>
-                    <p></p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="https://bing.nxingcloud.co/api/?day=2" class="d-block w-100 rounded" alt="bing">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5></h5>
-                    <p></p>
-                </div>
-            </div>
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
-
+    <?php
+    }
+    ?>
     <!-- 标题 -->
     <div id="pic-tit" class="container align-middle">
         <h4><strong>往日图片</strong> <span class="badge badge-info">HD</span></h4>
     </div>
-
     <!-- 图片 -->
     <div class="container" id="pic-list">
 
         <div id="pic-js" class="row text-center justify-content-between">
-            <div id="pic-js-son0" data-day="0" class="pic-item my-3 shadow-sm rounded">
+            <?php
+            for ($i=0; $i<=11; $i++)
+            {
+                $pnumber = ($p-1) * 12 + $i;
+                $picnumber = (string)$pnumber;
+                $picjs = "pic-js-son";
+                $picid = $picjs . $picnumber; // "pic-js-son0-11"
+                $pic1preix = "https://bing.nxingcloud.co/api/?thumbnail=1&day="; // img: src
+                $pic25preix = "https://bing.nxingcloud.co/api/?thumbnail=25&day="; // img: data-src
+                $pic1url_1 = $pic1preix . $picnumber;
+                $pic25url_1 = $pic25preix . $picnumber;
+            ?>
+            <div id="<?php echo $picid; ?>" data-day="<?php echo $picnumber; ?>" class="pic-item my-3 shadow-sm rounded">
                 <div class="progressive">
-                    <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=0"
-                        src="https://bing.nxingcloud.co/api/?thumbnail=1&day=0" alt="">
+                    <img class="rounded img-fluid preview lazy" data-src="<?php echo $pic25url_1; ?>"
+                        src="<?php echo $pic1url_1; ?>" alt="">
                 </div>
                 <p class="my-2"></p>
             </div>
-            <div id="pic-js-son1" data-day="1" class="pic-item my-3 shadow-sm rounded">
-                <div class="progressive">
-                    <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=1"
-                        src="https://bing.nxingcloud.co/api/?thumbnail=1&day=1" alt="">
-                </div>
-                <p class="my-2"></p>
-            </div>
-            <div id="pic-js-son2" data-day="2" class="pic-item my-3 shadow-sm rounded">
-                <div class="progressive">
-                    <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=2"
-                        src="https://bing.nxingcloud.co/api/?thumbnail=1&day=2" alt="">
-                </div>
-                <p class="my-2"></p>
-            </div>
-            <div id="pic-js-son3" data-day="3" class="pic-item my-3 shadow-sm rounded">
-                <div class="progressive">
-                    <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=3"
-                        src="https://bing.nxingcloud.co/api/?thumbnail=1&day=3" alt="">
-                </div>
-                <p class="my-2"></p>
-            </div>
-            <div id="pic-js-son4" data-day="4" class="pic-item my-3 shadow-sm rounded">
-                <div class="progressive">
-                    <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=4"
-                        src="https://bing.nxingcloud.co/api/?thumbnail=1&day=4" alt="">
-                </div>
-                <p class="my-2"></p>
-            </div>
-            <div id="pic-js-son5" data-day="5" class="pic-item my-3 shadow-sm rounded">
-                <div class="progressive">
-                    <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=5"
-                        src="https://bing.nxingcloud.co/api/?thumbnail=1&day=5" alt="">
-                </div>
-                <p class="my-2"></p>
-            </div>
-            <div id="pic-js-son6" data-day="6" class="pic-item my-3 shadow-sm rounded">
-                <div class="progressive">
-                    <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=6"
-                        src="https://bing.nxingcloud.co/api/?thumbnail=1&day=6" alt="">
-                </div>
-                <p class="my-2"></p>
-            </div>
-            <div id="pic-js-son7" data-day="7" class="pic-item my-3 shadow-sm rounded">
-                <div class="progressive">
-                    <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=7"
-                        src="https://bing.nxingcloud.co/api/?thumbnail=1&day=7" alt="">
-                </div>
-                <p class="my-2"></p>
-            </div>
-            <div id="pic-js-son8" data-day="8" class="pic-item my-3 shadow-sm rounded">
-                <div class="progressive">
-                    <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=8"
-                        src="https://bing.nxingcloud.co/api/?thumbnail=1&day=8" alt="">
-                </div>
-                <p class="my-2"></p>
-            </div>
-            <div id="pic-js-son9" data-day="9" class="pic-item my-3 shadow-sm rounded">
-                <div class="progressive">
-                    <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=9"
-                        src="https://bing.nxingcloud.co/api/?thumbnail=1&day=9" alt="">
-                </div>
-                <p class="my-2"></p>
-            </div>
-            <div id="pic-js-son10" data-day="10" class="pic-item my-3 shadow-sm rounded">
-                <div class="progressive">
-                    <img class="rounded img-fluid preview lazy"
-                        data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=10"
-                        src="https://bing.nxingcloud.co/api/?thumbnail=1&day=10" alt="">
-                </div>
-                <p class="my-2"></p>
-            </div>
-            <div id="pic-js-son11" data-day="11" class="pic-item my-3 shadow-sm rounded">
-                <div class="progressive">
-                    <img class="rounded img-fluid preview lazy"
-                        data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=11"
-                        src="https://bing.nxingcloud.co/api/?thumbnail=1&day=11" alt="">
-                </div>
-                <p class="my-2"></p>
-            </div>
+            <?php
+            }
+            ?>
         </div>
 
     </div>
-    <?php
-    }
-    ?>
-    <?php
-    if ($p)
-    {
-        $pnumber = ($p-1) * 3;
-        $picnumber = (string)$pnumber;
-        $picjs = "pic-js-son";
-        $picid = $picjs . $picnumber;
-        $pic1preix = "https://bing.nxingcloud.co/api/?thumbnail=1&day=";
-        $pic25preix = "https://bing.nxingcloud.co/api/?thumbnail=25&day=";
-        $pic1url_1 = $pic1preix . $picnumber;
-        $pic25url_1 = $pic25preix . $picnumber;
-    ?>
-        <!-- 图片 -->
-        <div class="container" id="pic-list">
-
-            <div id="pic-js" class="row text-center justify-content-between">
-                <div id="<?php echo $picid; ?>" data-day="<?php echo $picnumber; ?>" class="pic-item my-3 shadow-sm rounded">
-                    <div class="progressive">
-                        <img class="rounded img-fluid preview lazy" data-src="<?php echo $pic25url_1; ?>"
-                            src="<?php echo $pic1url_1; ?>" alt="">
-                    </div>
-                    <p class="my-2"></p>
-                </div>
-                <div id="pic-js-son1" data-day="1" class="pic-item my-3 shadow-sm rounded">
-                    <div class="progressive">
-                        <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=1"
-                            src="https://bing.nxingcloud.co/api/?thumbnail=1&day=1" alt="">
-                    </div>
-                    <p class="my-2"></p>
-                </div>
-                <div id="pic-js-son2" data-day="2" class="pic-item my-3 shadow-sm rounded">
-                    <div class="progressive">
-                        <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=2"
-                            src="https://bing.nxingcloud.co/api/?thumbnail=1&day=2" alt="">
-                    </div>
-                    <p class="my-2"></p>
-                </div>
-                <div id="pic-js-son3" data-day="3" class="pic-item my-3 shadow-sm rounded">
-                    <div class="progressive">
-                        <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=3"
-                            src="https://bing.nxingcloud.co/api/?thumbnail=1&day=3" alt="">
-                    </div>
-                    <p class="my-2"></p>
-                </div>
-                <div id="pic-js-son4" data-day="4" class="pic-item my-3 shadow-sm rounded">
-                    <div class="progressive">
-                        <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=4"
-                            src="https://bing.nxingcloud.co/api/?thumbnail=1&day=4" alt="">
-                    </div>
-                    <p class="my-2"></p>
-                </div>
-                <div id="pic-js-son5" data-day="5" class="pic-item my-3 shadow-sm rounded">
-                    <div class="progressive">
-                        <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=5"
-                            src="https://bing.nxingcloud.co/api/?thumbnail=1&day=5" alt="">
-                    </div>
-                    <p class="my-2"></p>
-                </div>
-                <div id="pic-js-son6" data-day="6" class="pic-item my-3 shadow-sm rounded">
-                    <div class="progressive">
-                        <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=6"
-                            src="https://bing.nxingcloud.co/api/?thumbnail=1&day=6" alt="">
-                    </div>
-                    <p class="my-2"></p>
-                </div>
-                <div id="pic-js-son7" data-day="7" class="pic-item my-3 shadow-sm rounded">
-                    <div class="progressive">
-                        <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=7"
-                            src="https://bing.nxingcloud.co/api/?thumbnail=1&day=7" alt="">
-                    </div>
-                    <p class="my-2"></p>
-                </div>
-                <div id="pic-js-son8" data-day="8" class="pic-item my-3 shadow-sm rounded">
-                    <div class="progressive">
-                        <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=8"
-                            src="https://bing.nxingcloud.co/api/?thumbnail=1&day=8" alt="">
-                    </div>
-                    <p class="my-2"></p>
-                </div>
-                <div id="pic-js-son9" data-day="9" class="pic-item my-3 shadow-sm rounded">
-                    <div class="progressive">
-                        <img class="rounded img-fluid preview lazy" data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=9"
-                            src="https://bing.nxingcloud.co/api/?thumbnail=1&day=9" alt="">
-                    </div>
-                    <p class="my-2"></p>
-                </div>
-                <div id="pic-js-son10" data-day="10" class="pic-item my-3 shadow-sm rounded">
-                    <div class="progressive">
-                        <img class="rounded img-fluid preview lazy"
-                            data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=10"
-                            src="https://bing.nxingcloud.co/api/?thumbnail=1&day=10" alt="">
-                    </div>
-                    <p class="my-2"></p>
-                </div>
-                <div id="pic-js-son11" data-day="11" class="pic-item my-3 shadow-sm rounded">
-                    <div class="progressive">
-                        <img class="rounded img-fluid preview lazy"
-                            data-src="https://bing.nxingcloud.co/api/?thumbnail=25&day=11"
-                            src="https://bing.nxingcloud.co/api/?thumbnail=1&day=11" alt="">
-                    </div>
-                    <p class="my-2"></p>
-                </div>
-            </div>
-
-        </div>
-    <?php
-    }
-    ?>
 
     <!-- 页脚 -->
     <footer class="container-fluid">
